4555eebcbb81f80ec155af4eec515c6b95b8ad9a
Stefan Seehafer <sea75300@yahoo.de>
2025-10-06 18:17:58 +0200

    Update dashboardContainersLoad.php

--- events/dashboardContainersLoad.php
+++ events/dashboardContainersLoad.php
@@ -4,14 +4,15 @@ namespace fpcm\modules\nkorg\extstats\events;
 
 final class dashboardContainersLoad extends \fpcm\module\event {
 
-    public function run()
+    public function run() : \fpcm\module\eventResult
     {
         if (!$this->getObject()->getOption('show_visitors')) {
             return $this->data;
         }
         
         $this->data[] = 'fpcm\modules\nkorg\extstats\models\dashContainerStats';
-        return $this->data;
+        
+        return (new \fpcm\module\eventResult())->setData($this->data);
     }
 
     public function init() : bool
